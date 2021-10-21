c0b21bc33aaf501e1cc7230240467b2bd13bf74d
EmiliaJurado <emily_9814@hotmail.com>
2021-10-21 12:28:33 -0500

    version1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Html5Component } from './html5/html5.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Html5Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/html5/html5.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-html5',
+  templateUrl: './html5.component.html',
+  styleUrls: ['./html5.component.css']
+})
+export class Html5Component {
+  public mostrarHTML5():string{
+return "Esto es html5";
+  }
+
+ 
+
+}
